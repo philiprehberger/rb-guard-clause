2bc035e53245387ad7a5ab0b39c0a801a1daba5b
Philip Rehberger <me@philiprehberger.com>
2018-05-04 23:55:49 -0600

    Standardize RuboCop config and auto-correct offenses

--- lib/philiprehberger/guard_clause/guard.rb
+++ lib/philiprehberger/guard_clause/guard.rb
@@ -28,9 +28,7 @@ module Philiprehberger
       # @param message [String] custom error message
       # @return [Guard] self for chaining
       def not_nil(message = nil)
-        if @value.nil?
-          handle_violation(message || 'value must not be nil')
-        end
+        handle_violation(message || 'value must not be nil') if @value.nil?
         self
       end
 
@@ -39,9 +37,7 @@ module Philiprehberger
       # @param message [String] custom error message
       # @return [Guard] self for chaining
       def not_empty(message = nil)
-        if @value.respond_to?(:empty?) && @value.empty?
-          handle_violation(message || 'value must not be empty')
-        end
+        handle_violation(message || 'value must not be empty') if @value.respond_to?(:empty?) && @value.empty?
         self
       end
 
@@ -50,9 +46,7 @@ module Philiprehberger
       # @param message [String] custom error message
       # @return [Guard] self for chaining
       def positive(message = nil)
-        if @value.respond_to?(:>) && !(@value > 0)
-          handle_violation(message || 'value must be positive')
-        end
+        handle_violation(message || 'value must be positive') if @value.respond_to?(:>) && @value <= 0
         self
       end
 
@@ -62,7 +56,7 @@ module Philiprehberger
       # @param message [String] custom error message
       # @return [Guard] self for chaining
       def gte(n, message = nil)
-        if @value.respond_to?(:>=) && !(@value >= n)
+        if @value.respond_to?(:>=) && @value < n
           handle_violation(message || "value must be greater than or equal to #{n}")
         end
         self
@@ -74,9 +68,7 @@ module Philiprehberger
       # @param message [String] custom error message
       # @return [Guard] self for chaining
       def lte(n, message = nil)
-        if @value.respond_to?(:<=) && !(@value <= n)
-          handle_violation(message || "value must be less than or equal to #{n}")
-        end
+        handle_violation(message || "value must be less than or equal to #{n}") if @value.respond_to?(:<=) && @value > n
         self
       end
 
@@ -86,9 +78,7 @@ module Philiprehberger
       # @param message [String] custom error message
       # @return [Guard] self for chaining
       def matches(regex, message = nil)
-        unless regex.match?(@value.to_s)
-          handle_violation(message || "value must match #{regex.inspect}")
-        end
+        handle_violation(message || "value must match #{regex.inspect}") unless regex.match?(@value.to_s)
         self
       end
 
@@ -98,9 +88,7 @@ module Philiprehberger
       # @param message [String] custom error message
       # @return [Guard] self for chaining
       def one_of(arr, message = nil)
-        unless arr.include?(@value)
-          handle_violation(message || "value must be one of #{arr.inspect}")
-        end
+        handle_violation(message || "value must be one of #{arr.inspect}") unless arr.include?(@value)
         self
       end
 
@@ -110,9 +98,7 @@ module Philiprehberger
       # @param message [String] custom error message
       # @return [Guard] self for chaining
       def not_equal(other, message = nil)
-        if @value == other
-          handle_violation(message || "value must not be equal to #{other.inspect}")
-        end
+        handle_violation(message || "value must not be equal to #{other.inspect}") if @value == other
         self
       end
 
@@ -120,11 +106,9 @@ module Philiprehberger
 
       # @param message [String] the violation message
       def handle_violation(message)
-        if @soft
-          @errors << message
-        else
-          raise GuardClause::Error, message
-        end
+        raise GuardClause::Error, message unless @soft
+
+        @errors << message
       end
     end
   end
